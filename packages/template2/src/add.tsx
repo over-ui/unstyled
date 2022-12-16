655c48d390abee56064946e41cbdd79708541054
otterp012 <otterp012@gmail.com>
2022-12-16 20:12:01 +0900

    feat: actions

--- packages/template2/src/add.tsx
+++
@@ -1,3 +0,0 @@
-export const Div = () => {
-  return <div>123112312312312323</div>;
-};
--- packages/template2/src/index.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react";
-import React from "react";
-
-describe("button", () => {
-  it("render", () => {
-    expect(1 + 2).toBe(3);
-  });
-});
