c03dd091604552ddfaf2f7f7d4a59f7ffd1bd82a
ver <luzverde0314@gmail.com>
2022-12-18 00:34:55 +0900

    feat: radioGroupContext 생성

--- packages/radioGroup/src/RadioGroup.tsx
+++ packages/radioGroup/src/RadioGroup.tsx
@@ -1,3 +1,29 @@
-export const Button = () => {
-	return <button>수정</button>;
+import * as React from "react";
+import { Radio, RadioIndicator, RadioProps } from "./Radio";
+
+/* -------------------------------------------------------------------------------------------------
+ * RadioGroupContext
+ * -----------------------------------------------------------------------------------------------*/
+type RadioGroupContextValue = {
+	name?: string;
+	required?: boolean;
+	disabled?: boolean;
+	value?: string;
+	loop?: boolean;
+	onChange?(value: string): void;
+};
+
+const RadioGroupContext = React.createContext<RadioGroupContextValue>({});
+
+const RadioGroupProvider = (props: RadioGroupContextValue & { children: React.ReactNode }) => {
+	const { children, ...context } = props;
+	const value = context;
+
+	return <RadioGroupContext.Provider value={value}>{children}</RadioGroupContext.Provider>;
+};
+
+const useRadioGroupContext = () => {
+	const context = React.useContext(RadioGroupContext);
+
+	return context;
 };
