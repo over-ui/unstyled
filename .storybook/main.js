a6ce620c3aa27ef57d1a5c324d16ccf9d1e5a733
otterp012 <otterp012@gmail.com>
2023-01-09 20:09:05 +0900

    feat: stroybook #49

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  stories: ['../packages/**/src/*.stories.@(mdx|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+};
