57487de09151f0d7ef6f10e321bd9eec65dd7965
otterp012 <otterp012@gmail.com>
2023-01-09 20:09:05 +0900

    feat: stroybook #49

---
+++ packages/template/src/add.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { Button } from './add';
+import mdx from './add.mdx';
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Button',
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+  // mdx 추가하는 설정
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
