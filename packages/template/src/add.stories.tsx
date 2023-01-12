e248c7c4a4429f782b05f8f99cc02faecad0e7d8
otterp012 <otterp012@gmail.com>
2023-01-12 17:39:39 +0900

    chore: example 수정

--- packages/template/src/add.stories.tsx
+++ packages/template/src/add.stories.tsx
@@ -1,27 +1,38 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-
+import { ComponentStory, ComponentMeta, Meta } from '@storybook/react';
+import styled from 'styled-components';
 import { Button } from './add';
 import mdx from './add.mdx';
+
+const StyledButton = styled.button`
+  border: 0;
+  background-color: ${({ theme }) => theme.colors.main};
+  color: ${({ theme }) => theme.colors.text};
+  padding: 0.75em 1em;
+  border-radius: 0.5em;
+`;
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
-  component: Button,
+
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  component: Button,
   parameters: {
     docs: {
       page: mdx,
     },
   },
   // mdx 추가하는 설정
-} as ComponentMeta<typeof Button>;
+} as Meta;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button />;
 
-export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
+export const Second = () => {
+  return <div>Test</div>;
+};
+
+export const Styled = () => {
+  return <StyledButton>styled button</StyledButton>;
+};
