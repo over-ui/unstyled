f9c1b86504ea92a8100378a537057f128d48c7fc
ver <luzverde0314@gmail.com>
2023-01-13 14:04:09 +0900

    chore: dialog 초기 세팅

---
+++ packages/dialog/src/Dialog.stories.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { ComponentStory, ComponentMeta, Meta } from '@storybook/react';
+import styled from 'styled-components';
+import { Button } from './Dialog';
+// import mdx from './Dialog.mdx';
+
+const StyledButton = styled.button`
+  border: 0;
+  background-color: ${({ theme }) => theme.colors.main};
+  color: ${({ theme }) => theme.colors.text};
+  padding: 0.75em 1em;
+  border-radius: 0.5em;
+`;
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Example/Button',
+
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  component: Button,
+  parameters: {
+    docs: {
+      // page: mdx,
+    },
+  },
+  // mdx 추가하는 설정
+} as Meta;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+export const Second = () => {
+  return <div>Test</div>;
+};
+
+export const Styled = () => {
+  return <StyledButton>styled button</StyledButton>;
+};
