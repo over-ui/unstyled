bcf0afcf29850df7b62766b17f96ae0b17089391
ver <luzverde0314@gmail.com>
2023-01-15 23:02:39 +0900

    feat: dialog storybook 작성

--- packages/dialog/src/Dialog.stories.tsx
+++ packages/dialog/src/Dialog.stories.tsx
@@ -196,3 +196,23 @@ const StyledDescription = styled(Dialog.Description)({
 });
 
 const StyledClose = styled(Dialog.Close)({});
+
+export const CodeSandBox = () => {
+  return (
+    <>
+      <iframe
+        src="https://codesandbox.io/embed/clever-mendeleev-2lz7tn?fontsize=14&hidenavigation=1&theme=dark&editorsize=0&hidedevtools=1"
+        style={{
+          width: '100%',
+          height: '500px',
+          border: '0',
+          borderRadius: '4px',
+          overflow: 'hidden',
+        }}
+        title="clever-mendeleev-2lz7tn"
+        allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+        sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+      ></iframe>
+    </>
+  );
+};
