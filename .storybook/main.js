05e65274fe26a7cef02af70b5ae9767a8de157c7
otterp012 <otterp012@gmail.com>
2023-01-16 16:51:58 +0900

    fix: styled -> emotion

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../packages/**/src/*.stories.@(mdx|tsx)'],
+  stories: ['../packages/**/src/*.stories.tsx'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
