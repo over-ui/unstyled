22773e6077b2a98b42776ab48ca3ce3485461d81
otterp012 <otterp012@gmail.com>
2023-01-17 15:30:16 +0900

    chore: clean up

--- packages/toggle/src/toggle.stories.tsx
+++ packages/toggle/src/toggle.stories.tsx
@@ -1,5 +1,5 @@
-import React, { useRef, useState } from 'react';
-import { ComponentStory, ComponentMeta, Meta, Story } from '@storybook/react';
+import React, { useState } from 'react';
+import { Meta, Story } from '@storybook/react';
 import styled from '@emotion/styled';
 import { Toggle } from './toggle';
 import mdx from './toggle.mdx';
