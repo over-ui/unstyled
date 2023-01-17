64a8a95139d6ea53d9c0648efb556adbb62e0691
ver <luzverde0314@gmail.com>
2023-01-17 15:11:54 +0900

    chore: dialog 초기 세팅

---
+++ packages/dialog/src/Dialog.stories.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import styled from '@emotion/styled';
+import * as Dialog from './Dialog';
+import mdx from './Dialog.mdx';
+
+export default {
+  title: 'over-ui/dialog',
+  component: Dialog,
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+};
