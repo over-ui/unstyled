cc9dc1eaefef8f65bf71fb625b93bf0f6987d9dc
ver <luzverde0314@gmail.com>
2023-01-20 10:54:08 +0900

    feat: add radio

--- packages/radioGroup/src/Radio.tsx
+++ packages/radioGroup/src/Radio.tsx
@@ -1,131 +1,165 @@
-import * as React from "react";
+/* eslint-disable react/display-name */
+import * as React from 'react';
+import { Poly, composeEvent, useSafeContext } from '@over-ui/core';
 
 /* -------------------------------------------------------------------------------------------------
  * RadioContext
  * -----------------------------------------------------------------------------------------------*/
 type RadioContextValue = { checked?: boolean; disabled?: boolean };
 
-const RADIO_NAME = "Radio";
-
 const RadioContext = React.createContext<RadioContextValue>({});
 
 const RadioProvider = (props: RadioContextValue & { children: React.ReactNode }) => {
-	const { children, ...context } = props;
-	const value = context;
+  const { children, ...context } = props;
+  const value = context;
 
-	return <RadioContext.Provider value={value}>{children}</RadioContext.Provider>;
-};
-
-const useRadioContext = () => {
-	const context = React.useContext(RadioContext);
-
-	return context;
+  return <RadioContext.Provider value={value}>{children}</RadioContext.Provider>;
 };
 
 /* -------------------------------------------------------------------------------------------------
  * Radio
  * -----------------------------------------------------------------------------------------------*/
-type RadioElement = HTMLButtonElement;
-type RadioProps = {
-	checked?: boolean;
-	required?: boolean;
-	onCheck?(): void;
-} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+type RadioProps = Poly.Props<typeof RADIO_TAG> & {
+  checked?: boolean;
+  required?: boolean;
+  onCheck?(): void;
+};
 
-const Radio = React.forwardRef<RadioElement, RadioProps>((props, forwardedRef) => {
-	const { children, checked = true, disabled, required, value = "on", onCheck, ...radioProps } = props;
+const RADIO_TAG = 'button';
+const RADIO_DISPLAY_NAME = 'Radio';
 
-	return (
-		<RadioProvider checked={checked} disabled={disabled}>
-			<button
-				type="button"
-				role="radio"
-				aria-checked={checked}
-				disabled={disabled}
-				dats-state={getState(checked)}
-				value={value}
-				tabIndex={checked ? 0 : -1}
-				ref={forwardedRef}
-				onClick={() => {
-					if (!checked) onCheck?.();
-				}}
-				{...radioProps}
-			>
-				{children}
-			</button>
-			<ImplicitInput
-				value={value}
-				checked={checked}
-				disabled={disabled}
-				style={{
-					transform: "translateX(-100%)",
-				}}
-			/>
-		</RadioProvider>
-	);
-});
+const Radio: Poly.Component<typeof RADIO_TAG, RadioProps> = React.forwardRef(
+  <T extends React.ElementType = typeof RADIO_TAG>(
+    props: Poly.Props<T, RadioProps>,
+    forwardedRef: Poly.Ref<T>
+  ) => {
+    const {
+      as,
+      children,
+      checked = false,
+      disabled,
+      required,
+      value = 'on',
+      onCheck,
+      ...radioProps
+    } = props;
+    const Tag = as || RADIO_TAG;
 
-Radio.displayName = RADIO_NAME;
+    const handleOnClick = () => {
+      if (!checked) onCheck?.();
+    };
+
+    return (
+      <RadioProvider checked={checked} disabled={disabled}>
+        <Tag
+          type="button"
+          role="radio"
+          aria-checked={checked}
+          data-state={checked ? 'checked' : 'unchecked'}
+          data-disabled={disabled ? '' : undefined}
+          disabled={disabled}
+          value={value}
+          tabIndex={checked ? 0 : -1}
+          ref={forwardedRef}
+          onClick={composeEvent(handleOnClick, props.onClick)}
+          {...radioProps}
+        >
+          {children}
+        </Tag>
+        <ImplicitInput
+          value={value}
+          checked={checked}
+          required={required}
+          disabled={disabled}
+          style={{
+            transform: 'translateX(-100%)',
+          }}
+        />
+      </RadioProvider>
+    );
+  }
+);
+
+Radio.displayName = RADIO_DISPLAY_NAME;
 
 /* -------------------------------------------------------------------------------------------------
  * RadioIndicator
  * -----------------------------------------------------------------------------------------------*/
-const INDICATOR_NAME = "RadioIndicator";
+type RadioIndicatorProps = Poly.Props<typeof RADIO_INDICATOR_TAG>;
 
-type RadioIndicatorElement = HTMLSpanElement;
-type RadioIndicatorProps = React.HTMLProps<HTMLSpanElement>;
+const RADIO_INDICATOR_TAG = 'span';
+const RADIO_INDICATOR_DISPLAY_NAME = 'RadioIndicator';
 
-const RadioIndicator = React.forwardRef<RadioIndicatorElement, RadioIndicatorProps>((props, forwardedRef) => {
-	const context = useRadioContext();
+const RadioIndicator: Poly.Component<typeof RADIO_INDICATOR_TAG, RadioIndicatorProps> =
+  React.forwardRef(
+    <T extends React.ElementType = typeof RADIO_INDICATOR_TAG>(
+      props: Poly.Props<T, RadioIndicatorProps>,
+      forwardedRef: Poly.Ref<T>
+    ) => {
+      const { as, ...radioIndicatorProps } = props;
+      const Tag = as || RADIO_INDICATOR_TAG;
+      const context = useSafeContext(RadioContext, 'Radio.Indicator');
 
-	return (
-		<>
-			{context.checked && (
-				<span
-					data-state={getState(context.checked)}
-					data-disabled={context.disabled ? "" : undefined}
-					{...props}
-					ref={forwardedRef}
-				/>
-			)}
-		</>
-	);
-});
+      return (
+        <>
+          {context.checked && (
+            <Tag
+              data-state="checked"
+              data-disabled={context.disabled ? '' : undefined}
+              {...radioIndicatorProps}
+              ref={forwardedRef}
+            />
+          )}
+        </>
+      );
+    }
+  );
 
-RadioIndicator.displayName = INDICATOR_NAME;
+RadioIndicator.displayName = RADIO_INDICATOR_DISPLAY_NAME;
 
 /* -------------------------------------------------------------------------------------------------
  * ImplicitInput
  * -----------------------------------------------------------------------------------------------*/
-type ImplicitInputProps = {
-	checked: boolean;
-	disabled?: boolean;
-} & React.HTMLProps<HTMLInputElement>;
-
-const ImplicitInput = (props: ImplicitInputProps) => {
-	const { checked, ...inputProps } = props;
-
-	return (
-		<input
-			type="radio"
-			aria-hidden
-			defaultChecked={checked}
-			{...inputProps}
-			tabIndex={-1}
-			style={{
-				...props.style,
-				position: "absolute",
-				pointerEvents: "none",
-				opacity: 0,
-				margin: 0,
-			}}
-		/>
-	);
+type InputProps = Poly.Props<typeof IMPLICIT_INPUT_TAG>;
+type ImplicitInputProps = Omit<InputProps, 'checked'> & {
+  checked?: boolean;
 };
 
-function getState(checked: Boolean) {
-	return checked ? "checked" : "unchecked";
-}
+const IMPLICIT_INPUT_TAG = 'input';
 
-export { Radio, RadioIndicator };
+const ImplicitInput: Poly.Component<typeof IMPLICIT_INPUT_TAG, ImplicitInputProps> =
+  React.forwardRef(
+    <T extends React.ElementType = typeof IMPLICIT_INPUT_TAG>(
+      props: Poly.Props<T, ImplicitInputProps>,
+      forwardedRef: Poly.Ref<T>
+    ) => {
+      const { as, checked, ...inputProps } = props;
+      const Tag = as || IMPLICIT_INPUT_TAG;
+      const ref = React.useRef<HTMLInputElement>(null);
+
+      return (
+        <Tag
+          type="radio"
+          aria-hidden
+          defaultChecked={checked}
+          {...inputProps}
+          tabIndex={-1}
+          ref={ref}
+          style={{
+            ...props.style,
+            position: 'absolute',
+            pointerEvents: 'none',
+            opacity: 0,
+            margin: 0,
+          }}
+        />
+      );
+    }
+  );
+
+export {
+  //
+  Radio,
+  RadioIndicator,
+};
 export type { RadioProps };
